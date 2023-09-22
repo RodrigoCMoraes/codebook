f9f9afcb0b613aff13b9280cc0baca5eaf8d537a
RodrigoCMoraes <rodrigo.moraes@vtex.com.br>
2023-09-21 22:55:46 -0300

    add bfs using deque

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -26,4 +26,30 @@ def bfs(n, m, edges, s):
             distance[i] = -1
     del distance[s]
     del distance[0]
-    return distanc
+    return distance
+
+
+def bfs(graph, start):
+  # Initialize the queue and the set of visited nodes.
+  queue = []
+  visited = set()
+
+  # Add the starting node to the queue.
+  queue.append(start)
+
+  # While the queue is not empty:
+  while queue:
+
+    # Remove the next node from the queue.
+    node = queue.popleft()
+
+    # Mark the node as visited.
+    visited.add(node)
+
+    # Add the node's neighbors to the queue.
+    for neighbor in graph[node]:
+      if neighbor not in visited:
+        queue.append(neighbor)
+
+  # Return the list of visited nodes.
+  return list(visited)
